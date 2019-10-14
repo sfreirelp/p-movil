ce33e6f9265f3d8342b62899c898ca65a5bc8af5
sfreirelp <56525139+sfreirelp@users.noreply.github.com>
2019-10-13 23:00:23 -0300

    Update programa.ino

--- programa.ino
+++ programa.ino
@@ -149,17 +149,13 @@ void CERRAR() {
 
 void DISTANCIA () {
 
-  // Clears the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
 
-  // Calculating the distance
   distance = duration * 0.034 / 2; //NOTA: LUEGO FIJARSE A CUÁNTO EQUIVALE LA DISTANCIA DE 10 CM EN VALOR SIN PROCESAR - PROCESÁNDOLO DEMORA MUCHO EN CALCULARLO Y DA ERRORES (COMO UN VALOR REPENTINAMENTE ALTO)
 
 }
